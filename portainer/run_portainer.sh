19661b37c5d80ae6348cc736c2af34e65d4f69c4
middlefitting <wjdtmdcjf5@gmail.com>
2025-04-08 14:14:18 +0900

    과제 3. docker 컨테이너 GUI 관리 툴인 portainer를 설치하고 웹에서 접속하여 모니터링한다. url 참고 : https://docs.portainer.io/start/install-ce/server/docker/linux 웹 포트는 40005로 expose 한다 ( https 9443 포트 변경 필요 ). 웹브라우저 접속은 https://(본인VM Public IP):40005 admin 비밀번호 신규로 생성 (12자리 이상) 한다.

---
+++ portainer/run_portainer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker run -d \
+  --name portainer \
+  -p 40005:9443 \
+  -p 8000:8000 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
+  portainer/portainer-ce:latest
